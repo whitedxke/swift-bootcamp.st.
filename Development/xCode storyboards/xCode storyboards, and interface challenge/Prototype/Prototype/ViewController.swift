cfd1fe22970fb941a681008f4faf26a0436b9b34
eextjcxltuires <whitedxke@gmail.com>
2025-04-24 23:04:06 +0300

    xCode storyboards, and interface challenge.

---
+++ Development/xCode storyboards/xCode storyboards, and interface challenge/Prototype/Prototype/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Prototype
+//
+//  Created by Pavel Betenya on 24.04.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
